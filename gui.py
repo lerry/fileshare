88fdcb56179ee74e814beea9dac9430a4817dd5f
Lerry <lvdachao@gmail.com>
2011-12-07 13:33:01 +0800

    update gitignore file, make a gui.py to implement gui

---
+++ gui.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+'''
+For:
+by Lerry  http://lerry.org
+Start from 2011-12-07 13:13
+Last edit at 2011-12-07 13:13
+'''
+from PyQt4 import QtGui
+from PyQt4 import QtCore
+
+class Demo():
+    ${0}
