bc9548663bfbb84b5afe4ffde56325ac3c181fff
Lerry <lvdachao@gmail.com>
2011-12-11 20:21:44 +0800

    update some example file

--- test/xc.py
+++ test/xc.py
@@ -6,5 +6,5 @@ Start from 2011/07/03 19:27:28
 Last edit at 2011/07/03
 '''
 from xmlrpclib import ServerProxy
-s = ServerProxy('http://localhost:724')
-print s.test(3)
+s = ServerProxy('http://localhost:1724')
+print s.getUptime()
--- test/xs.py
+++ test/xs.py
@@ -5,10 +5,13 @@ by Lerry  http://lerry.org
 Start from 2011/07/03 19:27:20
 Last edit at 2011/07/03
 '''
+import os
 from SimpleXMLRPCServer import SimpleXMLRPCServer
-xs = SimpleXMLRPCServer(('',724))
+xs = SimpleXMLRPCServer(('',1724))
 def test(x):
     return [1,2,3,x]
-    
+def getUptime():
+    return os.popen('uptime').read()
 xs.register_function(test)
-xs.serve_forever()    
+xs.register_function(getUptime)
+xs.serve_forever()
