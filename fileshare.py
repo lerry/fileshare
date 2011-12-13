79b74abdb664437d5a4ebbc4ca9bc4433ee72168
Lerry <lvdachao@gmail.com>
2011-12-13 15:37:15 +0800

    finish function hello()--introduce&get nodelist

--- fileshare.py
+++ fileshare.py
@@ -7,33 +7,66 @@ Last edit at 2011/07/03
 '''
 import uuid
 import time
+import socket
+import random
 import config
 from threading import Thread
 from SimpleXMLRPCServer import SimpleXMLRPCServer
+from xmlrpclib import ServerProxy
+from SocketServer import ThreadingMixIn
 
 TTL = config.TTL
 UUID = uuid.uuid1().get_hex()
-nodelist = [('127.0.0.1','724',UUID)]
+nodelist = [UUID:('127.0.0.1','724')]
+ip = socket.gethostbyname(socket.gethostname())
 port = config.PORT
+LIMIT = 50
 
+class ThreadXMLRPCServer(ThreadingMixIn, SimpleXMLRPCServer):
+    pass
 
 class Node(object):
     def __init__(self, port, nodelist):
         self.port = port
         self.nodelist = nodelist
+        self.UUID = UUID
         t = Thread(target=self.keepFind)
         t.setDaemon(1)
         t.start()
 
+    def _greeting(self):
+        for node in self.nodelist:
+            self._greet(node)
+
+    def _greet(self,node):
+        #get node's ip & port
+        nodeinfo = nodelist[node]
+        s = ServerProxy(('http://'+node[0], node[1]))
+        templist = s.hello((self.UUID:))
+        for item  in templist:
+
+
     def keepFind(self):
+        '''
+        maintain a node list
+        '''
         while 1:
             #break
             print 'I am runing'
             time.sleep(1)
-    def hello(self):
-        return 'hello'
+
+    def hello(self,info):
+        '''
+        introduce yourself to other node,
+        and check if he is online
+        '''
+        if info not in self.nodelist:
+            self.nodelist.append(info)
+        random.shuffle(self.nodelist)
+        return self.nodelist[:LIMIT]
+
     def _start(self):
-        s = SimpleXMLRPCServer(('',self.port))
+        s = ThreadXMLRPCServer(('',self.port))
         s.register_instance(self)
         s.serve_forever()
 
