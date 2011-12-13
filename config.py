5ff25cb53a3d9b65bd450d5b940f84060d12c9db
Lerry <lvdachao@gmail.com>
2011-12-13 14:42:06 +0800

    begin to use threading

--- config.py
+++ config.py
@@ -5,4 +5,5 @@ by Lerry  http://lerry.org
 Start from 2011/07/27 22:42:06
 Last edit at 2011/07/27
 '''
-TTL = 6
+TTL = 5
+PORT = 1234
--- fileshare.py
+++ fileshare.py
@@ -6,22 +6,40 @@ Start from 2011/07/03 21:57:56
 Last edit at 2011/07/03
 '''
 import uuid
-import cofig
+import time
+import config
+from threading import Thread
 from SimpleXMLRPCServer import SimpleXMLRPCServer
 
-MAX_HISTORY_LENTH = 5
+TTL = config.TTL
 UUID = uuid.uuid1().get_hex()
 nodelist = [('127.0.0.1','724',UUID)]
+port = config.PORT
+
 
 class Node(object):
     def __init__(self, port, nodelist):
         self.port = port
         self.nodelist = nodelist
+        t = Thread(target=self.keepFind)
+        t.setDaemon(1)
+        t.start()
+
+    def keepFind(self):
+        while 1:
+            #break
+            print 'I am runing'
+            time.sleep(1)
     def hello(self):
-        pass
+        return 'hello'
+    def _start(self):
+        s = SimpleXMLRPCServer(('',self.port))
+        s.register_instance(self)
+        s.serve_forever()
 
 def main():
-    pass
+    n = Node(1234, nodelist)
+    n._start()
 
 
 if __name__ == "__main__":
