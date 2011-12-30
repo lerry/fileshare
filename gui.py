7da7eb08b2b845ec4d29594d17ef219b0144befc
Lerry <lvdachao@gmail.com>
2011-12-30 11:06:52 +0800

    start gui programing

--- gui.py
+++ gui.py
@@ -3,6 +3,39 @@
 '''
 For:
 by Lerry  http://lerry.org
-Start from 2011-12-21 17:49
-Last edit at 2011-12-21 17:49
+Start from 2011-12-29 10:29
+Last edit at 2011-12-29 10:49
 '''
+import sys
+from PyQt4 import QtCore
+from PyQt4 import QtGui
+from ui_main import Ui_form
+
+
+class Demo(QtGui.QMainWindow):
+    def __init__(self, parent=None):
+        QtGui.QWidget.__init__(self, parent)
+        self.ui = Ui_form()
+        self.ui.setupUi(self)
+
+        self.ui.list.setSortingEnabled(1)
+        self.update_list()
+
+    def update_list(self):
+        item = ['OaK','Banana','Apple',' Orange','Grapes','Jayesh']
+        listItem = []
+        for lst in item:
+            listItem.append(QtGui.QListWidgetItem(lst))
+        for i in range(len(listItem)):
+            self.ui.list.insertItem(i+1,listItem[i])
+
+if __name__ == "__main__":
+    app = QtGui.QApplication(sys.argv)
+    demo = Demo()
+    demo.show()
+    sys.exit(app.exec_())
+
+
+
+
+
--- main.py
+++ main.py
@@ -6,15 +6,23 @@ by Lerry  http://lerry.org
 Start from 2011-12-21 17:49
 Last edit at 2011-12-21 17:49
 '''
-import config
+import sys
+from PyQt4 import QtGui
+from config import config
 from core import Node
 from modules import utils
-
-
+from threading import Thread
+from gui import Demo
 def main():
-    n = Node(1234, 'nodes.db', config.UUID)
+    n = Node(1234, 'nodes.db', config.get('uuid'))
     n._start()
 
 
 if __name__ == "__main__":
-    main()
+    core = Thread(target=main)
+    core.setDaemon(1)
+    core.start()
+    app = QtGui.QApplication(sys.argv)
+    demo = Demo()
+    demo.show()
+    sys.exit(app.exec_())
