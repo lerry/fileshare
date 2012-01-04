6cb8c3742144ecb8a4c7d9d2b2db64c850991fb4
Lerry <lvdachao@gmail.com>
2012-01-04 20:16:24 +0800

    add files for build exe

---
+++ setup.py
@@ -0,0 +1,7 @@
+from cx_Freeze import setup, Executable
+
+setup(
+        name = "SunP2P",
+        version = "0.1",
+        description = "SunP2P 1.0",
+        executables = [Executable("gui.py")])
